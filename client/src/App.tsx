91777e56f76b84a833e4571cc200c4f6f9f71ef2
cibikumar-mv <cibikumarv@gmail.com>
2023-10-19 21:54:48 +0530

    feat: Migrated to vite react

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Pages from "./pages/Pages";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Pages />
+    </Router>
+  );
+}
+
+export default App;
